1eaa9ba7a31b2c6cdf3df4e8467abab565fd7fb7
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 <80960745+woolozzx21@users.noreply.github.com>
2023-03-24 19:38:35 +0900

    hello-spring-first-commit

---
+++ hello-spring/src/main/java/com/example/hellospring/controller/HelloController.java
@@ -0,0 +1,49 @@
+package com.example.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    //진짜는 지금부터 만약에 데이터를 내놔라고 하면 -> api를 많이 쓰는 이유
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
