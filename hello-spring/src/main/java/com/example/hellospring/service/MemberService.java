b38fd157854cdb0d29632508b7baa9b375f68404
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 <80960745+woolozzx21@users.noreply.github.com>
2023-03-29 22:58:38 +0900

    Week2 commit

---
+++ hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository; // 직접 생성이 아니라 외부에서 넣어줌.
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원X
+        validateDupliateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDupliateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전제회원조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
