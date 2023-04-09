d4136273e7d7f5b8328370348440724bec796fb8
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 <80960745+woolozzx21@users.noreply.github.com>
2023-04-09 22:31:32 +0900

    hotpix : Add til the aop

--- hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,11 +1,14 @@
 package com.example.hellospring;
 
+import com.example.hellospring.repository.JdbcTemplateMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.w3c.dom.CDATASection;
 
+import javax.sql.DataSource;
 import javax.swing.plaf.PanelUI;
 
 @Configuration
@@ -18,7 +21,8 @@ public class SpringConfig{
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(DataSource);
     }
 
 }
